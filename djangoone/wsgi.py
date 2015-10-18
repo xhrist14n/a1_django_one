6ce0ba52a980d006a8af5c7e390fba6b234aaf4f
Developer <khrist14n@gmail.com>
2015-10-18 18:02:31 -0500

    project django

---
+++ djangoone/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for djangoone project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+from dj_static import Cling
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoone.settings")
+
+try:
+    application = get_wsgi_application()
+except:
+    application = Cling(get_wsgi_application())
