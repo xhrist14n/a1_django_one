0d909cf43814e32d0ebff08a7c39142f1f49bbdd
Developer <khrist14n@gmail.com>
2015-10-18 18:30:06 -0500

    project django

--- djangoone/settings.py
+++ djangoone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6bn_uxeic^1ph(nn7*nf$5d1349)sf^1e$u#d9&2xir*-^1i1t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -97,27 +97,19 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
 
 ###----------------------------------------------------------------
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
+##import dj_database_url
 
 ###DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
